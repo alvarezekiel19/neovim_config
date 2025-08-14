434f34ef2d471633de5aa41837271482b3530118
alvarezekiel19 <alvarezekiel19@gmail.com>
2025-08-14 12:55:11 +0800

    Added new color schemes.

--- init.lua
+++ init.lua
@@ -12,20 +12,23 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup {
-    require 'plugins.colortheme',       -- Load Colortheme
-    require 'plugins.neotree',          -- Load Neotree
-    require 'plugins.bufferline',       -- Load bufferline
-    require 'plugins.lualine',          -- Load Lualine
-    require 'plugins.treesitter',       -- Load treesitter
-    require 'plugins.telescope',        -- Load telescope
-    require 'plugins.lsp',              -- Load LSP
-    require 'plugins.autocompletion',   -- Load Autocompletion
-    require 'plugins.none-ls',          -- Load none-ls
-    require 'plugins.gitsigns',         -- Load gitsigns
+    require 'plugins.catppuccin', -- Load Catppuccin Theme
+    require 'plugins.tokyonight', -- Load Tokyonight Theme
+    require 'plugins.onedarkpro', -- Load OneDarkPro Theme
+    require 'plugins.neotree', -- Load Neotree
+    require 'plugins.bufferline', -- Load bufferline
+    require 'plugins.lualine', -- Load Lualine
+    require 'plugins.treesitter', -- Load treesitter
+    require 'plugins.telescope', -- Load telescope
+    require 'plugins.lsp', -- Load LSP
+    require 'plugins.autocompletion', -- Load Autocompletion
+    require 'plugins.none-ls', -- Load none-ls
+    require 'plugins.gitsigns', -- Load gitsigns
     require 'plugins.indent-blankline', -- Load indent-blankline
     -- require 'plugins.hardtime',         -- Load hardtime.nvim
-    require 'plugins.markdownpreview',  -- Load Markdown preview
-    require 'plugins.live-server',      -- Load live-server
+    require 'plugins.markdownpreview', -- Load Markdown preview
+    require 'plugins.live-server', -- Load live-server
+    require 'plugins.vimbegood', -- Load Vim-be-good
 }
 
 -- Start of Inline Blockline setup
