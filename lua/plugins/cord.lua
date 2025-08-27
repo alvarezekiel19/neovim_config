097b8d144506dc2e4bf35f5007f69a4847c7ee0b
alvarezekiel19 <alvarezekiel19@gmail.com>
2025-08-27 11:52:38 +0800

    Added cord.nvim plugin and configured custom editing message in PRC

--- lua/plugins/cord.lua
+++ lua/plugins/cord.lua
@@ -1,5 +1,35 @@
 return {
     'vyfor/cord.nvim',
     build = ':Cord update',
-    -- opts = {}
+
+    opts = {
+        display = {
+            theme = 'default',
+            flavor = 'dark',
+        },
+
+        variables = {
+            git_status = function(opts)
+                return git_branch
+            end,
+        },
+
+        text = {
+            editing = function(opts)
+                local git_branch = vim.fn.system('git branch --show-current'):gsub('\n', '')
+                if git_branch ~= '' then
+                    return string.format('Editing %s on %s - %s:%s', opts.filename, git_branch, opts.cursor_line,
+                        opts.cursor_char)
+                else
+                    return string.format('Editing %s - %s:%s', opts.filename, opts.cursor_line, opts.cursor_char)
+                end
+            end,
+        },
+
+        hooks = {
+            workspace_change = function(opts)
+                git_branch = vim.fn.system('git branch --show-current'):gsub('\n', '')
+            end,
+        },
+    },
 }
